3e2253fdcb0ff0dee73b6d20322aa09ddca5ae36
saleh alsaggaf <saa111954@hotmail.com>
2022-01-10 01:43:43 +0300

    setup env

---
+++ js/index.js
@@ -0,0 +1,11 @@
+let numberOfCourse = document.getElementById('numberOfCourse');
+let totalHourse = document.getElementById('totalHourse');
+let totalPoint = document.getElementById('totalPoint');
+
+let gradesCredits = document.getElementById('gradesCredits');
+
+function createCourseInput() {
+    console.log(numberOfCourse.value);
+    console.log(totalHourse.value);
+    console.log(totalPoint.value);
+}
